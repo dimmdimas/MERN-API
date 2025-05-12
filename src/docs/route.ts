d9acf87c1a8e0159ea3de59648e5ab87d223d579
dimmdimas <mdrdimas1@gmail.com>
2025-05-12 19:23:59 +0700

    fix swagger vercel

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,5 +1,5 @@
 import {Express} from 'express'
-import swaggerUi from 'swagger-ui-express'
+import SwaggerUi from 'swagger-ui-express'
 import swaggerOutput from './swagger.output.json'
 import fs from 'fs'
 import path from 'path' 
@@ -7,7 +7,7 @@ import path from 'path'
 export default function docs(app: Express) {
     const css = fs.readFileSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8")
 
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput, {
+    app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerOutput, {
         customCss: css
     }));
 }
